d0ca406cc35640525f53a24a151f8d2e8db470da
Robert Hardin <robert.hardin@asurion.com>
2015-01-27 12:00:03 -0600

    rounding out a little, releasing to the public soon to get feedback. haven't tested everything

--- test.js
+++ test.js
@@ -1,1 +1,6 @@
-var stats = require('./lib/statistics2');
+var stats = require('./lib/statistics2'),
+    z = stats.pnormaldist(1 - (1 - 0.975) / 2),
+    normal = stats.normaldist(0.27);
+
+console.log(z);
+console.log(normal); //0.60641987319804
